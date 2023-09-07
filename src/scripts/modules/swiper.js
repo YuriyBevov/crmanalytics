89ebcd600093386b96c5c4de27d3e3b2af22bb5a
yuriybevov <yuriybevov@gmail.com>
2023-09-07 15:14:20 +0300

    add footer

--- src/scripts/modules/swiper.js
+++ src/scripts/modules/swiper.js
@@ -1,35 +1,10 @@
 import Swiper, { Navigation, EffectCoverflow, EffectCards } from 'swiper';
 Swiper.use([Navigation,EffectCoverflow,EffectCards]);
 
-const sliders = document.querySelectorAll('.main-slider');
-
-if(sliders) {
-  sliders.forEach(slider => {
-    new Swiper(slider, {
-      slidesPerView: 1,
-      spaceBetween: 60,
-      loop: true,
-
-      navigation: {
-        nextEl: '.main-slider-container .swiper-button-next',
-        prevEl: '.main-slider-container .swiper-button-prev',
-      }
-    });
-  });
-};
-
 const clientSlider = document.querySelector('.clients-slider');
 
 if(clientSlider) {
   new Swiper(clientSlider, {
-    /*effect: "cards",
-    slidesPerView: 3,
-    cardsEffect: {
-      grabCursor: true,
-      perSlideOffset: 80,
-      perSlideRotate: 1,
-      rotate: false,
-      slideShadows: false*/
       effect: "coverflow",
       grabCursor: true,
       centeredSlides: true,
@@ -48,5 +23,23 @@ if(clientSlider) {
         nextEl: '.clients-slider-container .swiper-button-next',
         prevEl: '.clients-slider-container .swiper-button-prev',
       }
+  });
+}
+
+const mainSliders = document.querySelectorAll('.main-slider');
+if(mainSliders) {
+  mainSliders.forEach(slider => {
+    const nextBtn = slider.parentNode.querySelector('.swiper-button-next');
+    const prevBtn = slider.parentNode.querySelector('.swiper-button-prev');
+
+    new Swiper(slider, {
+        slidesPerView: 1,
+        spaceBetween: 30,
+
+        navigation: {
+          nextEl: nextBtn,
+          prevEl: prevBtn,
+        }
+    });
   });
 }
